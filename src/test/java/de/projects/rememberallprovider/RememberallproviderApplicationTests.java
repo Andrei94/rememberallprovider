631d3a00b9f9d1c287beb4e192fc3726dd48aaaf
Andrei Precup <andreiprecup69@gmail.com>
2018-11-25 15:49:33 +0200

    Junit 5.

--- src/test/java/de/projects/rememberallprovider/RememberallproviderApplicationTests.java
+++ src/test/java/de/projects/rememberallprovider/RememberallproviderApplicationTests.java
@@ -1,16 +1,44 @@
 package de.projects.rememberallprovider;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class RememberallproviderApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RememberallproviderApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private RemarkRepository repository;
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	public void contextLoads() {
+	void persistRemark() {
+		assertThat(restTemplate.postForEntity("http://localhost:" + port + "/remarks",
+				new Remark("test", "positive"), ResponseEntity.class).getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
+	@Test
+	void getAllRemarks() {
+		restTemplate.postForEntity("http://localhost:" + port + "/remarks",
+				new Remark("test", "positive"), ResponseEntity.class);
+		restTemplate.postForEntity("http://localhost:" + port + "/remarks",
+				new Remark("test2", "negative"), ResponseEntity.class);
+		assertThat(restTemplate.getForEntity("http://localhost:" + port + "/allRemarks",
+				RemarksList.class).getBody()).isEqualTo(new RemarksList(new Remark("test", "positive"), new Remark("test2", "negative")));
+	}
+
+	@AfterEach
+	void cleanTest() {
+		repository.deleteAll();
+	}
 }
