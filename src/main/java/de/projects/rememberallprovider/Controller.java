06465029a42b4ba3ddff464c4e8c6c7d5c55c7b4
Andrei Precup <andreiprecup69@gmail.com>
2018-11-28 23:17:32 +0200

    /remarks endpoint now allows more than one remark per request.

--- src/main/java/de/projects/rememberallprovider/Controller.java
+++ src/main/java/de/projects/rememberallprovider/Controller.java
@@ -3,11 +3,11 @@ package de.projects.rememberallprovider;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class Controller {
 	private final RemarkRepository repository;
 
@@ -16,12 +16,13 @@ public class Controller {
 	}
 
 	@PostMapping("/remarks")
-	public ResponseEntity<String> submitRemark(@RequestBody Remark remark) {
-		repository.save(new RemarkEntity(remark.getDescription(), remark.getQuality()));
+	public ResponseEntity<String> submitRemark(@RequestBody List<Remark> remarks) {
+		repository.saveAll(remarks.stream().map(remark -> new RemarkEntity(remark.getDescription(), remark.getQuality())).collect(Collectors.toList()));
 		return ResponseEntity.ok().build();
 	}
 
 	@GetMapping("/allRemarks")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public ResponseEntity<RemarksList> getAllRemarks() {
 		return ResponseEntity.of(Optional.of(new RemarksList(repository.findAll().stream()
 				.map(entity -> new Remark(entity.getDescription(), entity.getQuality())).collect(Collectors.toList()))));
