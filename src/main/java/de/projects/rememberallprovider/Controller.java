66813ff280d9ad9bad8b302a22479a451baf25f0
Andrei Precup <andreiprecup69@gmail.com>
2018-11-28 19:14:49 +0200

    Add CORS for http://localhost:4200.

--- src/main/java/de/projects/rememberallprovider/Controller.java
+++ src/main/java/de/projects/rememberallprovider/Controller.java
@@ -1,15 +1,13 @@
 package de.projects.rememberallprovider;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class Controller {
 	private final RemarkRepository repository;
 
